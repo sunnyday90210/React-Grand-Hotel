129ce3ac62ba7a3a57c57469af54886be979c7ef
Robert Wollny <robertjohnwollny@gmail.com>
2019-01-02 20:58:27 -0800

    added smooth scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import './Navbar.css';
 
 class Navbar extends Component {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+  scrollToBottom = () => {
+    scroll.scrollToBottom();
+  };
+
+  scrollToAbout = () => {
+    scroll.scrollTo(950);
+  };
+
+  scrollToRooms = () => {
+    scroll.scrollTo(1550);
+  };
+
   render() {
     let navbarClasses = 'navigation';
     if (this.props.show) {
@@ -26,34 +42,58 @@ class Navbar extends Component {
         </div>
         <ul className="navigation-list">
           <li className="navigation-item">
-            <a href="/" className="navigation-link">
-              Home
-            </a>
+            <Link
+              id="home"
+              className="navigation-link"
+              to="home"
+              onClick={this.scrollToTop}>
+              home
+            </Link>
           </li>
           <li className="navigation-item">
-            <a href="/" className="navigation-link">
+            <Link
+              id="aboutus"
+              className="navigation-link"
+              to="aboutus"
+              onClick={this.scrollToAbout}>
               about us
-            </a>
+            </Link>
           </li>
           <li className="navigation-item">
-            <a href="/" className="navigation-link">
+            <Link
+              id="rooms"
+              className="navigation-link"
+              to="rooms"
+              onClick={this.scrollToRooms}>
               rooms
-            </a>
+            </Link>
           </li>
           <li className="navigation-item">
-            <a href="/" className="navigation-link">
+            <Link
+              id="events"
+              className="navigation-link"
+              to="events"
+              onClick={this.scrollToTop}>
               events
-            </a>
+            </Link>
           </li>
           <li className="navigation-item">
-            <a href="/" className="navigation-link">
+            <Link
+              id="customers"
+              className="navigation-link"
+              to="customers"
+              onClick={this.scrollToBottom}>
               customers
-            </a>
+            </Link>
           </li>
           <li className="navigation-item">
-            <a href="/" className="navigation-link">
+            <Link
+              id="contact"
+              className="navigation-link"
+              to="contact"
+              onClick={this.scrollToBottom}>
               contact
-            </a>
+            </Link>
           </li>
         </ul>
         <div className="copyright">
